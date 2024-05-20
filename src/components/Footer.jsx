9258756124147b4f9f376a65a8f8d357d5ce0b35
Zota0 <77154534+Zota0@users.noreply.github.com>
2024-05-20 10:44:12 +0200

    Footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,15 @@ import Image from "next/image";
 const Footer = () => {
 
     return (
-        <div className="w-screen bottom-0 absolute flex flex-col h-14 justify-center align-middle items-center">
-            <div className="w-1/5 h-full"></div>
-            <div className="w-1/5 h-full">Skontaktuj się na: zota0.dev@gmail.com</div>
-            <div className="w-1/5 h-full"></div>
-            <div className="w-1/5 h-full"></div>
-            <div className="w-1/5 h-full"></div>
+        <div className="flex flex-row justify-center align-middle text-center items-center h-14 w-screen overflow-hidden bottom-0 absolute">
+            <div className="h-[calc(10%)] bg-white w-full"></div>
+            <div className="bg-gray-800 h-[calc(90%)] w-full flex flex-col justify-center align-middle items-center">
+                <div className="w-1/5 h-full"></div>
+                <div className="w-1/5 h-full">Skontaktuj się na: zota0.dev@gmail.com</div>
+                <div className="w-1/5 h-full"></div>
+                <div className="w-1/5 h-full"></div>
+                <div className="w-1/5 h-full"></div>
+            </div>
         </div>
     );
 }
