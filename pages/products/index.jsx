67c9b520a895447b8d909c96e767049d5b9fa007
Zota0 <77154534+Zota0@users.noreply.github.com>
2024-05-21 12:01:37 +0000

    list style product

--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -17,9 +17,37 @@ export default function ServerSide(props) {
                 <br />
                 <div className='w-full h-full flex justify-between gap-8 align-middle items-center text-center flex-row'>
                     {
+                        // (SSR_Data && SSR_Data.length > 0 ? SSR_Data.map((item) => (
+                        //     <Link key={item.id} href={`/products/${item.id}`} className='w-64 h-64'>
+                        //         <div className='aspect-square border-2 border-black bg-black flex justify-center gap-0.5 items-center h-full w-full overflow-hidden flex-row'>
+                        //             <div>
+                        //                 <div className='text-center h-full w-full'>
+                        //                     {item.title}
+                        //                 </div>
+                        //             </div>
+                        //             <div>
+                        //                 <div className='overflow-hidden h-full w-full'>
+                        //                     <img
+                        //                         src={item.thumbnail}
+                        //                         alt={item.title + `image`}
+                        //                         width={1024}
+                        //                         height={1024}
+                        //                         className='h-full w-full'
+                        //                     />
+                        //                 </div>
+                        //             </div>
+                        //             <div>
+                        //                 <div className='text-center h-full w-full'>
+                        //                     {item.price + `zł`}
+                        //                 </div>
+                        //             </div>
+                        //         </div>
+                        //     </Link>
+                        // )) : null)
+
                         (SSR_Data && SSR_Data.length > 0 ? SSR_Data.map((item) => (
-                            <Link key={item.id} href={`/products/${item.id}`} className='w-64 h-64'>
-                                <div className='aspect-square border-2 border-black bg-black flex justify-center gap-0.5 items-center h-full w-full overflow-hidden flex-row'>
+                            <Link key={item.id} href={`/products/${item.id}`} className='w-full h-96'>
+                                <div className='border-2 border-black bg-black flex justify-center gap-0.5 items-center h-full w-full overflow-hidden flex-row'>
                                     <div>
                                         <div className='text-center h-full w-full'>
                                             {item.title}
