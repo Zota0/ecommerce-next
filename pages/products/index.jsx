f58dd861d9dea087ae58cfa258dc45be37ee17a7
Zota0 <kontakt.pcgamerpl6.yt@gmail.com>
2024-05-22 15:52:06 +0200

    list and grid types

--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -1,46 +1,53 @@
-import { Image } from 'next/image';
 
-const Products = () => {
+const ApiPath = 'https://dummyjson.com/products';
+import React, { useEffect, useState, cache } from 'react';
+import Link from 'next/link';
+
+export default function ServerSide(props) {
+
+    const [ProductViewType, setProductViewType] = useState('list');
+    const { getServerSideData } = props;
+    console.log(getServerSideData);
+    const SSR_Data = getServerSideData.products;
 
     return (
         <>
             <h1>Wszystkie produkty</h1>
+            <div className='w-full h-14 bg-black'>
+                <div className='w-full h-full'>
+                    <div className='w-1/12 h-full'></div>
+                    <div className='w-1/12 h-full'></div>
+                    <div className='w-1/12 h-full'></div>
+                    <div className='w-1/12 h-full'></div>
+                    <div className='w-1/12 h-full'></div>
+                    <div className='w-1/12 h-full'></div>
+                    <div className='w-1/12 h-full'></div>
+                    <div className='w-1/12 h-full'></div>
+                    <div className='w-1/12 h-full'></div>
+                    <div className='w-1/12 h-full'></div>
+                    <div className='w-1/12 h-full'></div>
+                    <div className='w-1/12 h-full'>
+                        <div className='w-full h-full flex justify-center items-center text-white gap-0'>
+                            <button className='w-1/2 h-full' onClick={() => setProductViewType('grid')}>
+                                {(ProductViewType == 'grid') ? (<span className='text-lime-500'>Grid</span>) : (<span className='text-red-500'>Grid</span>)
+                                }
+                            </button>
+                            <button className='w-1/2 h-full' onClick={() => setProductViewType('list')}>
+                                {(ProductViewType == 'list') ? (<span className='text-lime-500'>List</span>) : (<span className='text-red-500'>List</span>)
+                                }
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <br />
             <div className='w-full flex-row'>
                 <br />
-                <div className='w-full h-full flex justify-between gap-8 align-middle items-center text-center flex-row'>
+                <div className='w-full h-full flex justify-center gap-8 align-middle items-center text-center flex-row'>
                     {
-                        // (SSR_Data && SSR_Data.length > 0 ? SSR_Data.map((item) => (
-                        //     <Link key={item.id} href={`/products/${item.id}`} className='w-64 h-64'>
-                        //         <div className='aspect-square border-2 border-black bg-black flex justify-center gap-0.5 items-center h-full w-full overflow-hidden flex-row'>
-                        //             <div>
-                        //                 <div className='text-center h-full w-full'>
-                        //                     {item.title}
-                        //                 </div>
-                        //             </div>
-                        //             <div>
-                        //                 <div className='overflow-hidden h-full w-full'>
-                        //                     <img
-                        //                         src={item.thumbnail}
-                        //                         alt={item.title + `image`}
-                        //                         width={1024}
-                        //                         height={1024}
-                        //                         className='h-full w-full'
-                        //                     />
-                        //                 </div>
-                        //             </div>
-                        //             <div>
-                        //                 <div className='text-center h-full w-full'>
-                        //                     {item.price + `zł`}
-                        //                 </div>
-                        //             </div>
-                        //         </div>
-                        //     </Link>
-                        // )) : null)
-
-                        (SSR_Data && SSR_Data.length > 0 ? SSR_Data.map((item) => (
-                            <Link key={item.id} href={`/products/${item.id}`} className='w-full h-96'>
-                                <div className='border-2 border-black bg-black flex justify-center gap-0.5 items-center h-full w-full overflow-hidden flex-row'>
+                        ((ProductViewType=='grid') ? ((SSR_Data && SSR_Data.length > 0 ? SSR_Data.map((item) => (
+                            <Link key={item.id} href={`/products/${item.id}`} className='w-64 h-64'>
+                                <div className='aspect-square border-2 border-black bg-black flex justify-center gap-0.5 items-center h-full w-full overflow-hidden flex-row'>
                                     <div>
                                         <div className='text-center h-full w-full'>
                                             {item.title}
@@ -65,6 +72,34 @@ const Products = () => {
                                 </div>
                             </Link>
                         )) : null)
+                        ) : ((SSR_Data && SSR_Data.length > 0 ? SSR_Data.map((item) => (
+                            <Link key={item.id} href={`/products/${item.id}`} className='w-[calc(80%)] h-64'>
+                                <div className='aspect-square border-2 border-black bg-black flex justify-center gap-0.5 items-center h-full w-full overflow-hidden flex-row'>
+                                    <div>
+                                        <div className='text-center h-full w-full'>
+                                            {item.title}
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className='overflow-hidden h-full w-full'>
+                                            <img
+                                                src={item.thumbnail}
+                                                alt={item.title + `image`}
+                                                width={1024}
+                                                height={1024}
+                                                className='h-full w-full'
+                                            />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className='text-center h-full w-full'>
+                                            {item.price + `zł`}
+                                        </div>
+                                    </div>
+                                </div>
+                            </Link>
+                        )) : null)
+                        ))
                     }
                 </div>
 
